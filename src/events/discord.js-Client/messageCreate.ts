d2a61b9198aada9c72987f4cfd70fb44c3c13c12
oogooro <70372978+oogooro@users.noreply.github.com>
2023-01-15 21:24:12 +0100

    Odpowiadanie na wzmianki

--- src/events/discord.js-Client/messageCreate.ts
+++ src/events/discord.js-Client/messageCreate.ts
@@ -1,3 +1,4 @@
+import { ButtonStyle, ComponentType, OAuth2Scopes, PermissionsBitField } from 'discord.js';
 import { betaServers, client, logger } from '../..';
 import { botSettingsDB } from '../../database/botSettings';
 import { DjsClientEvent } from '../../structures/DjsClientEvent';
@@ -5,6 +6,28 @@ import { DjsClientEvent } from '../../structures/DjsClientEvent';
 export default new DjsClientEvent('messageCreate', async message => {
     if (message.author.bot) return;
     const { devs } = botSettingsDB.get(process.env.ENV);
+
+    if (message.content.replace('!', '') === `<@${client.user.id}>`) {
+        const invite = client.generateInvite({ scopes: [OAuth2Scopes.Bot], permissions: [PermissionsBitField.Flags.Connect, PermissionsBitField.Flags.Speak, PermissionsBitField.Flags.ViewChannel], });
+
+        return message.reply({
+            content: `Witaj!\nJestem botem muzycznym!\nWszystkie moje komendy znajdziesz wpisująć **/**`,
+            components: [{
+                type: ComponentType.ActionRow,
+                components: [
+                    {
+                        type: ComponentType.Button,
+                        style: ButtonStyle.Link,
+                        url: invite,
+                        label: 'Zaproś na serwer',
+                    },
+                ],
+            }],
+            allowedMentions: {
+                parse: [ 'users', ],
+            },
+        }).catch(err => logger.error(err));
+    }
 
     if (devs.includes(message.author.id) && message.mentions.has(client.user)) {
         if (message.inGuild()) {
