810abab7602404e70514a0c636d6ef189a635ac2
oogooro <70372978+oogooro@users.noreply.github.com>
2023-01-18 11:05:00 +0100

    Wychodzenie z kanału, gdy bot zostanie sam

---
+++ src/events/discord.js-Client/voiceStateUpdate.ts
@@ -0,0 +1,31 @@
+import { logger, queues } from '../..';
+import { DjsClientEvent } from '../../structures/DjsClientEvent';
+
+const INACTIVITY_PERIOD = 5 * 1000 * 60; /* 5 min */
+
+const activeTimeouts: Map<string, NodeJS.Timeout> = new Map();
+
+export default new DjsClientEvent('voiceStateUpdate', async (oldState, newState) => {
+    if (!queues.has(oldState.guild.id)) return;
+    const queue = queues.get(oldState.guild.id);
+    const channel = queue.voiceChannel;
+
+    const guildId = queue.guild.id;
+
+    if ((channel.id === oldState.channelId) && oldState.channelId !== newState.channelId) {
+        if (channel.members.size === 1) {
+            logger.debug(`[INACTIVITY] ${guildId} Only bot's left, starting timer`);
+
+            if (activeTimeouts.has(guildId)) clearTimeout(activeTimeouts.get(guildId));
+            activeTimeouts.set(guildId, setTimeout(() => {
+                logger.debug(`[INACTIVITY] ${guildId} Left due to inactivity`);
+                queue.byebye();
+                activeTimeouts.delete(guildId);
+            }, INACTIVITY_PERIOD));
+        }
+    } else if (activeTimeouts.has(guildId) && (channel.id === newState.channelId) && (oldState.channelId !== newState.channelId)) {
+        logger.debug(`[INACTIVITY] ${guildId} Someone joined, clearing timer`);
+        clearTimeout(activeTimeouts.get(guildId));
+        activeTimeouts.delete(guildId);
+    }
+});
