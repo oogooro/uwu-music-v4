872e61ea94359b3df3ff940c24ae173d35c368ae
oogooro <70372978+oogooro@users.noreply.github.com>
2023-04-02 12:00:01 +0200

    Naprawienie głosności bota

--- src/structures/AudioPlayerManager.ts
+++ src/structures/AudioPlayerManager.ts
@@ -140,7 +140,8 @@ export class AudioPlayerManager {
             seekTime = seekTime ?? 0;
             stream(song.url, { seek: seekTime, quality: 2 })
                 .then(ytStream => {
-                    const resource = createAudioResource(ytStream.stream, { inputType: ytStream.type });
+                    const resource = createAudioResource(ytStream.stream, { inputType: ytStream.type, inlineVolume: true, });
+                    resource.volume.setVolume(0.5);
                     this.currentResource = resource;
                     this.seekOffset = seekTime;
             
@@ -178,7 +179,8 @@ export class AudioPlayerManager {
                 });
 
                 soundcloud.util.streamTrack(song.url).then(stream => {
-                    const resource = createAudioResource(pipeline(stream, transcoder, () => void 0), { inputType: StreamType.Raw, });
+                    const resource = createAudioResource(pipeline(stream, transcoder, () => void 0), { inputType: StreamType.Raw, inlineVolume: true, });
+                    resource.volume.setVolume(0.5);
                     this.currentResource = resource;
                     this.seekOffset = seekTime;
 
@@ -234,7 +236,8 @@ export class AudioPlayerManager {
                     responseType: 'stream',
                 });
 
-                const resource = createAudioResource(pipeline(stream, transcoder, () => void 0), { inputType: StreamType.Raw, });
+                const resource = createAudioResource(pipeline(stream, transcoder, () => void 0), { inputType: StreamType.Raw, inlineVolume: true, });
+                resource.volume.setVolume(0.5);
                 this.currentResource = resource;
                 this.seekOffset = seekTime;
 
