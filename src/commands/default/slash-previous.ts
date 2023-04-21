92f73945d9510f1a2994885aa7ea08f23657bd28
oogooro <70372978+oogooro@users.noreply.github.com>
2023-04-21 16:02:01 +0200

    Naprawa i zablokowanie /previous

--- src/commands/default/slash-previous.ts
+++ src/commands/default/slash-previous.ts
@@ -1,13 +1,12 @@
-import { InteractionEditReplyOptions, InteractionReplyOptions } from 'discord.js';
-import config from '../../config';
+import { InteractionEditReplyOptions } from 'discord.js';
 import { SlashCommand } from '../../structures/SlashCommand';
 import { Song } from '../../structures/Song';
 import { SoundcloudSong } from '../../structures/SoundcoludSong';
 import { YoutubeSong } from '../../structures/YoutubeSong';
 import { RepeatMode } from '../../typings/repeatMode';
-import { createSongEmbed, songToDisplayString } from '../../utils';
+import { createSongEmbed } from '../../utils';
 
-export default new SlashCommand({
+export default new SlashCommand({ //! THIS COMMAND IS DISABLED
     data: {
         name: 'previous',
         description: 'Gra poprzednią piosenkę',
@@ -15,6 +14,7 @@ export default new SlashCommand({
     },
     vcOnly: true,
     queueRequired: true,
+    disabled: true, //! THIS COMMAND IS DISABLED
     run: async ({ interaction, logger, queue }) => {
         const interactionResponse = await interaction.deferReply().catch(err => { logger.error(err) });
         if (!interactionResponse) return;
@@ -24,9 +24,9 @@ export default new SlashCommand({
         if (queue.repeatMode !== RepeatMode.Queue) {
             song = queue.previousSongs.pop();
             
-            if (!song) return interaction.reply({ content: 'Brak piosenki do zagrania!' }).catch(err => logger.error(err));
+            if (!song) return interaction.editReply({ content: 'Brak piosenki do zagrania!' }).catch(err => logger.error(err));
         } else {
-            if (queue.songs.length < 1) return interaction.reply({ content: 'Brak piosenki do zagrania!' }).catch(err => logger.error(err));
+            if (queue.songs.length < 1) return interaction.editReply({ content: 'Brak piosenki do zagrania!' }).catch(err => logger.error(err));
             song = queue.songs.pop();
         }
 
