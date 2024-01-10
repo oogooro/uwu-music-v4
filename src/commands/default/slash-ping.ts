d6145124413fe7066b2f3cb987ac9142965bb2ad
oogooro <70372978+oogooro@users.noreply.github.com>
2024-01-10 16:14:01 +0100

    Pong

--- src/commands/default/slash-ping.ts
+++ src/commands/default/slash-ping.ts
@@ -9,12 +9,12 @@ export default new SlashCommand({
     },
     global: true,
     run: async ({ interaction, logger }) => {
-        interaction.reply({ content: `:ping_pong: **Ping!**\nWebsocket: ${client.ws.ping}ms\nInterakcje: ...`, fetchReply: true, }).then(async msg => {
+        interaction.reply({ content: `:ping_pong: **Pong!**\nWebsocket: ${client.ws.ping}ms\nInterakcje: ...`, fetchReply: true, }).then(async msg => {
             const channel = await client.channels.fetch(interaction.channelId) as TextBasedChannel;
 
             const time = (await channel.messages.fetch(msg.id)).createdTimestamp - interaction.createdTimestamp;
 
-            interaction.editReply({ content: `:ping_pong: **Ping!**\nWebsocket: ${client.ws.ping}ms\nInterakcje: ${time}ms`, })
+            interaction.editReply({ content: `:ping_pong: **Pong!**\nWebsocket: ${client.ws.ping}ms\nInterakcje: ${time}ms`, })
                 .catch(err => logger.error(err));
         }).catch(err => logger.error(err));
     },
